28a94d9bc90cc28cff7c05b469b407ac14f97b4a
TejasSurse <tejassunilsurse2004@gmail.com>
2025-01-17 16:59:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './sections/navbar';
+const App = () => {
+  return(
+    <main className='max-w-7xl mx-auto'>
+     <Navbar></Navbar>
+     </main>
+  )
+}
+
+export default App;
+
+
+1.35
